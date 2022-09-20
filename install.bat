5295175af2f95778f2bc93a1a7e46b5da5e7e9a3
Keshuac <48709680+Keshuac@users.noreply.github.com>
2022-09-21 00:05:57 +1200

    Removed tests from install.bat

--- install.bat
+++ install.bat
@@ -1,13 +1,11 @@
-@REM @echo off
+@echo off
 setlocal enableextensions 
 
 set location="%USERPROFILE%\AppData\Local\Vivaldi\Application\vivaldi.exe"
-@REM for /F "delims=" %%i in () do set test=%%i
 powershell -noprofile "(Get-Item -path %USERPROFILE%\AppData\Local\Vivaldi\Application\vivaldi.exe).VersionInfo.ProductVersion" > temp.txt
 set /p version=<temp.txt
 del temp.txt
 set source="%cd%\Contents\"
-@REM set test="%cd%\test\"
 set destination="%USERPROFILE%\AppData\Local\Vivaldi\Application\%version%\resources\vivaldi\user_files\"
 
 xcopy /s/e %source% %destination%
